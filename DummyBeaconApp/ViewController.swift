9f382d890a22a07b02a5d0cde6b0dc02f08dd640
Daniel Sánchez Seijo <daniseijo@MacBook-Pro-de-Daniel.local>
2015-04-22 19:49:14 +0200

    Initial Commit

---
+++ DummyBeaconApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DummyBeaconApp
+//
+//  Created by Daniel Sánchez Seijo on 22/4/15.
+//  Copyright (c) 2015 Daniel Sánchez Seijo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

eb72b759fdae811bbfd42e482d54c3c9bcd6f42c
Daniel Seijo Sánchez <daniseijo12@gmail.com>
2015-08-29 20:16:33 +0200

    Se acerca o aleja la imagen en funcion de la proximidad que da el accuracy del iBeacon

--- DummyBeaconApp/RangeViewController.swift
+++ DummyBeaconApp/RangeViewController.swift
@@ -13,6 +13,13 @@ class RangeViewController: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
     let region = CLBeaconRegion(proximityUUID: NSUUID(UUIDString: "E2C56DB5-DFFB-48D2-B060-D0F5A71096E0"), identifier: "Estimotes");
+    let colors = [
+        1: UIColor(red: 84/255, green: 77/255, blue: 160/255, alpha: 1),
+        2: UIColor(red: 142/255, green: 212/255, blue: 220/255, alpha: 1),
+        3: UIColor(red: 162/255, green: 213/255, blue: 181/255, alpha: 1)
+    ];
+    
+    @IBOutlet weak var proximityImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +42,18 @@ class RangeViewController: UIViewController, CLLocationManagerDelegate {
     // MARK: - Location Manager Delegate
     
     func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
-        println(beacons);
+        //println(beacons);
+        let knownBeacons = beacons.filter{ $0.proximity != CLProximity.Unknown }
+        
+        if (knownBeacons.count > 0) {
+            let closestBeacon = knownBeacons[0] as! CLBeacon;
+            var frame:CGRect = self.proximityImage.frame
+            println(self.view.frame.size.height + CGFloat(closestBeacon.accuracy.distanceTo(0.0)));
+            //println(closestBeacon.accuracy.distanceTo(0.0));
+            frame.origin.y = (self.view.frame.size.height - 60) + CGFloat((60*closestBeacon.accuracy.distanceTo(0.0)));
+            self.proximityImage.frame = frame
+            //self.view.backgroundColor = self.colors[closestBeacon.minor.integerValue]
+        }
     }
 
     /*

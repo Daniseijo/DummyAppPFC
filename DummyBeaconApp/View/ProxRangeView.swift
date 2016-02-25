e850fa21d87e7565444999599332503951cf4bd4
Daniel Seijo Sánchez <daniseijo12@gmail.com>
2016-02-25 20:58:50 +0100

    Seguida la guía de estilo

--- DummyBeaconApp/View/ProxRangeView.swift
+++ DummyBeaconApp/View/ProxRangeView.swift
@@ -9,36 +9,36 @@
 import UIKit
 
 class ProxRangeView: UIView {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    self.backgroundColor = UIColor.clearColor()
+  }
+  
+  required init(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // Only override drawRect: if you perform custom drawing.
+  // An empty implementation adversely affects performance during animation.
+  override func drawRect(rect: CGRect) {
+    // Get the Graphics Context
+    let context = UIGraphicsGetCurrentContext();
     
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        self.backgroundColor = UIColor.clearColor()
-    }
+    // Set the circle outerline-width
+    CGContextSetLineWidth(context, 1.0);
     
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    // Set the circle colors
+    UIColor.redColor().setStroke()
+    UIColor.blueColor().setFill()
     
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Get the Graphics Context
-        let context = UIGraphicsGetCurrentContext();
-        
-        // Set the circle outerline-width
-        CGContextSetLineWidth(context, 1.0);
-        
-        // Set the circle colors
-        UIColor.redColor().setStroke()
-        UIColor.blueColor().setFill()
-        
-        // Create Circle
-        CGContextAddArc(context, frame.size.width / 2, frame.size.height / 2, (frame.size.width - 10) / 2, 0.0, CGFloat(M_PI * 2.0), 1)
-        // Fill circle with color
-        CGContextDrawPath(context, CGPathDrawingMode.Fill);
-        
-        // Draw
-        CGContextStrokePath(context);
-    }
+    // Create Circle
+    CGContextAddArc(context, frame.size.width / 2, frame.size.height / 2, (frame.size.width - 10) / 2, 0.0, CGFloat(M_PI * 2.0), 1)
+    // Fill circle with color
+    CGContextDrawPath(context, CGPathDrawingMode.Fill);
     
+    // Draw
+    CGContextStrokePath(context);
+  }
+  
 }
